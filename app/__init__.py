6caf069b9f8ae0046549bfb65de0e33bcfa441be
IanoSang <ianosang18@gmail.com>
2022-05-11 01:48:23 +0300

    feat: Authentication- adds a login and a logot pages

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,12 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pomodoro.db'
-app.config['SECRET_KEY'] = 'ed96823c766c274dc3bd19e0'
+app.config['SECRET_KEY'] = 'group3'
+db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 
-db = SQLAlchemy(app)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from app import app
 from flask import render_template, redirect, url_for, flash
 from app.models import User
-from app.forms import RegisterForm
+from app.forms import RegisterForm, LoginForm
 from app import db
-from flask_login import login_user
+from flask_login import login_user, logout_user, login_required
 
 
 @app.route('/')
@@ -13,23 +13,48 @@ def home_page():
 
 
 @app.route('/timer')
+@login_required
 def timer_page():
 
     return render_template('timer.html')
 
 
-@app.route('/register', methods=['GET', 'POST'])
-def register_page():
+@app.route('/signup', methods=['GET', 'POST'])
+def signup_page():
     form = RegisterForm()
     if form.validate_on_submit():
-        user_to_create = User(username=form.username.data,
-                              email_address=form.email_address.data,
-                              password=form.password1.data)
+        user_to_create = User(username=form.username.data,email_address=form.email_address.data,password=form.password1.data)
         db.session.add(user_to_create)
         db.session.commit()
-        return redirect(url_for('market_page'))
+        login_user(user_to_create)
+        flash(f"Account created successfully! You are now logged in as {user_to_create.username}", category='success')
+        return redirect(url_for('timer_page'))
     if form.errors != {}:  # If there are not errors from the validations
         for err_msg in form.errors.values():
             flash(f'There was an error with creating a user: {err_msg}', category='danger')
 
     return render_template('signup.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login_page():
+    form = LoginForm()
+    if form.validate_on_submit():
+        attempted_user = User.query.filter_by(username=form.username.data).first()
+        if attempted_user and attempted_user.check_password_correction(
+                attempted_password=form.password.data
+        ):
+            login_user(attempted_user)
+            flash(f'Success! You are logged in as: {attempted_user.username}', category='success')
+            return redirect(url_for('timer_page'))
+        else:
+            flash('Username and password are not match! Please try again', category='danger')
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+def logout_page():
+    logout_user()
+    flash("You have been logged out!", category='info')
+    return redirect(url_for("home_page"))
